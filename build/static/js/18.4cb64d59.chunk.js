591a260b9d26dcf6ecf7178f184f2b4a2db14e48
arjun <arjun@method-hub.com>
2021-04-25 14:36:09 +0530

    add component

---
+++ build/static/js/18.4cb64d59.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@coreui/coreui-free-react-admin-template"]=this["webpackJsonp@coreui/coreui-free-react-admin-template"]||[]).push([[18],{627:function(e,t,c){"use strict"},693:function(e,t,c){"use strict";c.r(t);c(1);var r=c(618),i=(c(617),c(627),c(20));t.default=function(e){e.match;return Object(i.jsx)(r.J,{children:"add users    "})}}}]);
+//# sourceMappingURL=18.4cb64d59.chunk.js.map
