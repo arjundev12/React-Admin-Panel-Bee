d36409bc0c15c4006fe2654f4018965061565494
arjun <arjun@method-hub.com>
2021-04-29 14:13:43 +0530

    add update in customer

--- build/static/js/19.c0ca3888.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@coreui/coreui-free-react-admin-template"]=this["webpackJsonp@coreui/coreui-free-react-admin-template"]||[]).push([[19],{630:function(e,t,c){"use strict"},693:function(e,t,c){"use strict";c.r(t);c(1);var r=c(618),i=(c(617),c(630),c(20));t.default=function(e){e.match;return Object(i.jsx)(r.J,{children:"add users    "})}}}]);
-//# sourceMappingURL=19.c0ca3888.chunk.js.map
