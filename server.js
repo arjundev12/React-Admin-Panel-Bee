667af1e6a071f61bee02d34bd4b3ed16f624bf53
arjun <arjun@method-hub.com>
2021-04-29 13:52:57 +0530

    add update in customer

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const port = 3003;
 
 console.log(".....", path.join(__dirname, "build"))
 app.use(express.static(path.join(__dirname, "build")));
+// app.use( ,express.static(path.join(__dirname, "build")));
 
 app.listen(port, () => {
     console.log(`server listening on port ${port}`)
